c4f4a21e90ad4f0d3b25d9adbbf3a87b7adaf532
itskaramaman <45313619+itskaramaman@users.noreply.github.com>
2019-05-10 14:32:37 +0530

    "question added"

--- staircase.py
+++ staircase.py
@@ -1,3 +1,16 @@
+#Simran is running up a staircase with N steps, and can hop(jump) either 1 step, 3 steps or 5 steps at a time.
+#You have to count, how many possible ways Simran can run up to the stairs.
+
+#Input Format:
+#Input contains integer N that is number of steps
+
+#Output Format:
+#Output for each integer N the no of possible ways w.
+
+#Constraints
+#0<n<30
+
+
 steps=int(input())
 def numways(steps):
 	if steps==0 or steps==1:
